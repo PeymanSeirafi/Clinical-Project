d12c670a3d97faddd0a522c108b2c61a7a3cd1b0
Mp <parsasheini87@gmail.com>
2024-06-29 22:22:38 +0330

    Fixed Statics & Urls ⚙️

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
 from django.urls import path
-from .views import LoginPage
+from .views import LoginPage, DoctorsPage, ModerationPage, ServicesPage, VisitsPage, HelpPage
 
 urlpatterns = [
-    path("login", LoginPage.as_view(), name='home')
+    path('panel/login', LoginPage.as_view(), name='login'),
+    path('panel/doctors', DoctorsPage.as_view(), name='doctors'),
+    path('panel/moderation', ModerationPage.as_view(), name='mod'),
+    path('panel/services', ServicesPage.as_view(), name='services'),
+    path('panel/visits', VisitsPage.as_view(), name='visits'),
+    path('panel/help', HelpPage.as_view(), name='help'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-# Create your views here.
+# Admin Pages
+
 class LoginPage(APIView):
     def get(self, request):
         return Response(template_name='admin-panel/admin-login.html')
+
+class DoctorsPage(APIView):
+    def get(self, request):
+        return Response(template_name='admin-panel/admin-doctors.html')
+
+class ModerationPage(APIView):
+    def get(self, request):
+        return Response(template_name='admin-panel/admin-moderation.html')
+
+class ServicesPage(APIView):
+    def get(self, request):
+        return Response(template_name='admin-panel/admin-services.html')
+
+class VisitsPage(APIView):
+    def get(self, request):
+        return Response(template_name='admin-panel/admin-visits.html')
+
+class HelpPage(APIView):
+    def get(self, request):
+        return Response(template_name='admin-panel/help.html')
