35d49bf5c9ecdf615eeff31ea803d15049005002
Mp <parsasheini87@gmail.com>
2024-07-03 13:48:11 +0330

    Completed Models... ✨

--- main/models.py
+++ main/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
-class Visit(models.Model):
-  full_name = models.CharField(max_length=100)
-  city = models.CharField(max_length=100)
-  phone_number = models.CharField(max_length=11)
-  # service = models.TextChoices()
-  time = models.CharField(max_length=100)
-
 class Doctor(models.Model):
   full_name = models.CharField(max_length=100)
   desc = models.TextField()
   photo = models.ImageField(default='default-avatar.jpeg', upload_to='doctors')
 
+  def __str__(self): return self.full_name
+
 class Service(models.Model):
   title = models.CharField(max_length=100)
   desc = models.TextField()
   photo = models.ImageField(upload_to='services')
+
+  def __str__(self): return self.title
+
+class Visit(models.Model):
+  full_name = models.CharField(max_length=100)
+  city = models.CharField(max_length=100)
+  phone_number = models.CharField(max_length=11)
+  service = models.CharField(max_length=225, choices=[(obj.id, obj.title) for obj in Service.objects.all()])
+  time = models.CharField(max_length=100)
+
+  def get_choices(self): print(self.Service_Choices)
