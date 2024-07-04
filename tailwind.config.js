ffd727c266961ab74d94f91cc80c9889a16a1820
Peyman Seirafi <91782017+PeymanSeirafi@users.noreply.github.com>
2024-07-04 15:12:36 +0330

    Changing login page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./project_statics/admin-panel/js/**/*.js",
-    "./project_statics/js/**/*.js",
-    "./project_statics/npm-files/node_modules/flowbite/**/*.js",
-    "./main/templates/**/*.html",
-  ],
+  content: ["./**/*.{html, js}"],
   theme: {
     extend: {},
   },
   // plugins: [require("flowbite/plugin")],
 };
 
-// npx tailwindcss -i ./project_statics/css/input.css -o ./project_statics/css/output.css --watch
+// npx tailwindcss -i ./static_files/project_statics/css/input.css -o ./static_files/project_statics/css/output.css --watch
